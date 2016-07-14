eda876ab9aa0e9462e50f77d1e4afb1f387021bf
Jordan Marano <jordan@campbellsci.com.au>
2016-07-14 11:23:27 +1000

    Expanded Example Code
    Expanded the example code to build a bigger, more variable and realistic
    message.

--- LoRaAT/examples/Send_Data/Send_Data.ino
+++ LoRaAT/examples/Send_Data/Send_Data.ino
@@ -15,6 +15,8 @@
   --------------------------------------------------------------------------------------*/
 #include <LoRaAT.h>               //Include LoRa AT libraray
 #include <SoftwareSerial.h>       //Software serial for debug
+#include <Wire.h>
+#include "DS3231.h"
 
 /*--------------------------------------------------------------------------------------
   Definitions
@@ -22,6 +24,7 @@
 
 SoftwareSerial debugSerial(10, 11);     // RX, TX
 LoRaAT mdot(0, &debugSerial);           //Instantiate a LoRaAT object
+DS3231 RTC;                             //Create the DS3231 object
 
 /*--- setup() --------------------------------------------------------------------------
   Called by the Arduino framework once, before the main loop begins.
@@ -34,6 +37,9 @@ void setup() {
   
   debugSerial.begin(38400);   //Debug output. Listen on this ports for debugging info
   mdot.begin(38400);          //Begin (possibly amongst other things) opens serial comms with MDOT
+  Wire.begin();
+  RTC.begin();
+  
   debugSerial.println(F("\r\n\r\n++ START ++\r\n\r\n"));
 
 
@@ -48,20 +54,86 @@ void setup() {
 /*--- loop() ---------------------------------------------------------------------------
   Main loop called by the Arduino framework
   --------------------------------------------------------------------------------------*/
-int count = 1;
+int loopNum = 0;
 void loop() {
   int responseCode;         //Response code from the mdot
-  //String testMessage = "";  //Test message sent via debugSerial
-  //String temp PROGMEM = "Temp:";
-  //String rh PROGMEM = ",RH:";
-  String testMsg = "test1:-127.00,test2:202,test3:119,test7:12.7,test8:86,test9:6";
+  String testMessage = "";  //Test message sent via debugSerial
+  
+  //Build the message to send:
+  String rtcTemp = F("RTC Temp:");
+  testMessage = rtcTemp;
+  RTC.convertTemperature(); //convert current temperature into registers
+  testMessage += RTC.getTemperature();
 
-  debugSerial.println("MAIN  : testMsg: " + testMsg);
+  String atTemp = F("ATmega328P Temp:");
+  testMessage += atTemp;
+  testMessage += GetTemp();
+  
+  String count = F(",Loop Count:");
+  testMessage += count;
+  testMessage += loopNum;
 
-  responseCode = mdot.sendPairs(testMsg);
+  DateTime now = RTC.now(); //get the current date-time
+  String Year = F(",Year:");
+  testMessage += Year;
+  testMessage += now.year();
+  
+  String Month = F(",Month:");
+  testMessage += Month;
+  testMessage += now.month();
+  
+  String Day = F(",Day:");
+  testMessage += Day;
+  testMessage += now.date();
+
+  String Hour = F(",Hour:");
+  testMessage += Hour;
+  testMessage += now.hour();
+  
+  String Min = F(",Min:");
+  testMessage += Min;
+  testMessage += now.minute();
+  
+  String Sec = F(",Sec:");
+  testMessage += Sec;
+  testMessage += now.second();
+  
+  debugSerial.println("MAIN  : testMsg: " + testMessage);
+
+  responseCode = mdot.sendPairs(testMessage);
 
   debugSerial.println("MAIN  : send result: " + String(responseCode));
 
   delay(30000);
-  count++;
+  loopNum++;
+}
+
+double GetTemp(void) {
+  unsigned int wADC;
+  double t;
+
+  // The internal temperature has to be used
+  // with the internal reference of 1.1V.
+  // Channel 8 can not be selected with
+  // the analogRead function yet.
+
+  // Set the internal reference and mux.
+  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+  ADCSRA |= _BV(ADEN);  // enable the ADC
+
+  delay(20);            // wait for voltages to become stable.
+
+  ADCSRA |= _BV(ADSC);  // Start the ADC
+
+  // Detect end-of-conversion
+  while (bit_is_set(ADCSRA,ADSC));
+
+  // Reading register "ADCW" takes care of how to read ADCL and ADCH.
+  wADC = ADCW;
+
+  // The offset of 324.31 could be wrong. It is just an indication.
+  t = (wADC - 324.31 ) / 1.22;
+
+  // The returned temperature is in degrees Celsius.
+  return (t);
 }
