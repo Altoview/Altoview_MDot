603ad95f3c23f794b0d5d7e168cbd0c53143e58e
marno11 <jordan.marano11@gmail.com>
2016-07-15 14:54:53 +1000

    Toyed Around With a find function

--- LoRaAT/LoRaAT.h
+++ LoRaAT/LoRaAT.h
@@ -36,15 +36,15 @@ class LoRaAT
 	void begin();			        //Use default baud
 	void begin(uint32_t);	        //Use specified baud rate.
 	
-	int join();				        //Join a LoRa network.
-	int join(unsigned int);         //Join with a specific timeout
-    void leave();			        //Leave a LoRa network, Not yet implemented
+	//int join();				        //Join a LoRa network.
+	//int join(unsigned int);         //Join with a specific timeout
+    //void leave();			        //Leave a LoRa network, Not yet implemented
 	
-	int send(char*);		        //Generic send command, using AT+SEND
-	int send(char*, unsigned int);  //Use specific timeout.
-	int sendPairs(char*);	        //Takes key,value pairs, forms a message, and sends to the LoRa server.
-    int sendPairs(String); 	        //Takes key,value pairs, forms a message, and sends to the LoRa server.
-	uint8_t ping();			        //Not yet implemented
+	//int send(char*);		        //Generic send command, using AT+SEND
+	//int send(char*, unsigned int);  //Use specific timeout.
+	//int sendPairs(char*);	        //Takes key,value pairs, forms a message, and sends to the LoRa server.
+    //int sendPairs(String); 	        //Takes key,value pairs, forms a message, and sends to the LoRa server.
+	//uint8_t ping();			        //Not yet implemented
 	
 	//Getter and setter functions for mDot Settings.
 	//NOT FULLY TESTED AND IMPLEMENTED
@@ -63,17 +63,17 @@ class LoRaAT
     int commitSettings();			//Not yet implemented
   
   private:
-	static const uint8_t _MAX_FRAGMENTS = 16;
-	static const uint8_t _PACKET_SIZE = 11;
-	static const uint8_t _HEADER_SIZE = 2;
-	static const uint8_t _PAYLOAD_SIZE = _PACKET_SIZE - _HEADER_SIZE;
-	static const uint8_t _HEADER_OFFSET = 48;
+	//static const uint8_t _MAX_FRAGMENTS = 16;
+	//static const uint8_t _PACKET_SIZE = 11;
+	//static const uint8_t _HEADER_SIZE = 2;
+	//static const uint8_t _PAYLOAD_SIZE = _PACKET_SIZE - _HEADER_SIZE;
+	//static const uint8_t _HEADER_OFFSET = 48;
     
-	static const uint8_t _MAX_PAIRS_SIZE = 100;
+	//static const uint8_t _MAX_PAIRS_SIZE = 100;
     
-	char _txBuffer[_MAX_FRAGMENTS][_PACKET_SIZE];
-	uint8_t _txPutter = 0;
-	uint8_t _txGetter = 0;
+	//char _txBuffer[_MAX_FRAGMENTS][_PACKET_SIZE];
+	//uint8_t _txPutter = 0;
+	//uint8_t _txGetter = 0;
 	
 	static const uint8_t _MAX_MDOT_RESPONSE = 150;			//Max number of bytes the mdot might return
 	char _response[_MAX_MDOT_RESPONSE];						//mDot response buffer
@@ -85,9 +85,11 @@ class LoRaAT
 	uint8_t _sendCommand(char*, char*, char*, char*, char*, uint16_t);
 	uint8_t _sendCommand(char*, char*, char*, char*, char*, uint16_t, char*);             //Generic serial out get response wrapper
 	
-	void _pairsToJSON(char*, char*);
-	void _createFragmentBuffer(char*);
-	int _processBuffer();
+	//void _pairsToJSON(char*, char*);
+	//void _createFragmentBuffer(char*);
+	//int _processBuffer();
+	
+	bool _find(char*, char*, char*);
 	
     uint8_t _u8SerialPort;                                 //AT Command serial port selection by user
 	Stream* _debugStream;                                  //Debugging serial port initialized in constructor
